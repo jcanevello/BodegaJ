868e0f87e69f1dc86985049cd87261e9dd09b186
jcane <jean.canevello@gmail.com>
2015-11-18 06:53:06 -0500

    cerrar sesion

--- application/views/header.php
+++ application/views/header.php
@@ -13,18 +13,18 @@
             <ul class="nav navbar-nav pull-right">
                 <li class="dropdown profile-dropdown">
                     <a href="#" class="dropdown-toggle" data-toggle="dropdown">
-                        <img src="/BodegaJ/media/img/samples/profile-photo.png" alt=""/>
-                        <span class="hidden-xs">Juan Perez</span> <b class="caret"></b>
+                        <!--<img src="/BodegaJ/media/img/samples/profile-photo.png" alt=""/>-->
+                        <span class="hidden-xs">Administrador</span> <b class="caret"></b>
                     </a>
                     <ul class="dropdown-menu">
                         <!--li><a href="user-profile.html"><i class="fa fa-user"></i>Profile</a></li-->
-                        <li><a href="#"><i class="fa fa-cog"></i>Configuración</a></li>
-                        <li><a href="#"><i class="fa fa-envelope-o"></i>Mensajes</a></li>
+<!--                        <li><a href="#"><i class="fa fa-cog"></i>Configuración</a></li>
+                        <li><a href="#"><i class="fa fa-envelope-o"></i>Mensajes</a></li>-->
                         <li><a href="/BodegaJ/login/logout"><i class="fa fa-power-off"></i>Cerrar sesión</a></li>
                     </ul>
                 </li>
                 <li class="hidden-xxs">
-                    <a class="btn">
+                    <a class="btn" href="/BodegaJ/login/logout">
                         <i class="fa fa-power-off"></i>
                     </a>
                 </li>
